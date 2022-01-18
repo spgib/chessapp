a948f1d830060663006bda14fcfbfa52e77e0e7e
Spencer <sp.gib3@gmail.com>
2022-01-18 18:24:08 +0100

    Added http-error model and error handling

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,16 @@ const gamesRoutes = require('./routes/games-routes');
 
 module.exports = () => {
   const app = express();
-  
+
   app.use(express.json());
-  
+
   app.use('/api/users', usersRoutes);
   app.use('/api/games', gamesRoutes);
 
+  app.use((error, req, res, next) => {
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred.' });
+  });
+
   return app;
-}
+};
