76233c1c629f951e4b0773d72b39b7df8a96a644
Spencer <sp.gib3@gmail.com>
2022-01-18 18:20:45 +0100

    Updated user repo

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -8,4 +8,28 @@ module.exports.postSignup = async (req, res, next) => {
   res.send(user);
 };
 
-module.exports.postLogin = (req, res, next) => {};
+module.exports.postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let user;
+  try {
+    user = await UserRepo.findByEmail(email);
+  } catch (err) {
+    return next(new Error('Login failed!'));
+  }
+
+  if (!user) {
+    return next(new Error('Invalid credentials, failed to log in!'));
+  }
+  
+  const isValidPassword = password === user.password;
+
+  if (!isValidPassword) {
+    return next(new Error('Invalid credentials, failed to log in!'));
+  }
+
+  res.status(200).json({
+    userId: user.id,
+    email: user.email
+  });
+};
--- backend/repos/user-repo.js
+++ backend/repos/user-repo.js
@@ -1,9 +1,21 @@
 const pool = require('../db/pool');
 
 class UserRepo {
-  static test() {
-    console.log('test');
+  static async find() {
+    const { rows } = await pool.query('SELECT * FROM users;');
+
+    return rows;
   }
+
+  static async findByEmail(email) {
+    const { rows } = await pool.query(
+      'SELECT id, email, password FROM users WHERE email = $1;',
+      [email]
+    );
+
+    return rows[0];
+  }
+
   static async insert(name, email, password) {
     const { rows } = await pool.query(
       'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;',
