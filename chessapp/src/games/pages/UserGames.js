a0b14d37db09f9f76eab6d39fa09aae0bafe3f73
Spencer <sp.gib3@gmail.com>
2022-01-24 16:15:07 +0100

    Connected user game list to backend

--- chessapp/src/games/pages/UserGames.js
+++ chessapp/src/games/pages/UserGames.js
@@ -1,47 +1,72 @@
-// import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 
-// import UserGame from '../components/UserGame';
-// import { AuthContext } from '../../store/context/auth-context';
+import UserGame from '../components/UserGame';
+import { AuthContext } from '../../store/context/auth-context';
 
+const UserGames = (props) => {
+  const [games, setGames] = useState([]);
+  const auth = useContext(AuthContext);
+  const params = useParams();
 
+  const uid = params.uid;
 
-// const UserGames = (props) => {
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const games = await fetch(
+          `http://localhost:5000/api/games/list/user/${uid}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + auth.token,
+            },
+          }
+        );
 
+        const gamesData = await games.json();
 
- 
+        if (!games.ok) {
+          throw new Error(gamesData.message);
+        }
+        
+        setGames(gamesData.userGames);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-//   // let content;
+    fetchGames();
+  }, [setGames, uid, auth.token]);
 
-//   // if (auth.games.length > 0) {
-//   //   content = auth.games.filter(game => game.userId === auth.userId).map((game) => {
-//   //     return (
-//   //       <UserGame
-//   //         id={game.id}
-//   //         key={game.id}
-//   //         title={game.title}
-//   //         turns={game.turns}
-//   //         wPlayer={game.wPlayer}
-//   //         bPlayer={game.bPlayer}
-//   //         checkmate={game.checkmate}
-//   //         resignation={game.resignation}
-//   //         winner={game.winner}
-//   //         description={game.description}
-//   //         onDelete={deleteGame}
-//   //         onReview={reviewGame}
-//   //         // onEdit={editGame}
-//   //         isUser={auth.isLoggedIn}
-//   //         public={game.public}
-//   //       />
-//   //     );
-//   //   });
-//   // }
+  let content;
+  if (games.length > 0) {
+    content = games.map((game) => {
+      return (
+        <UserGame
+          id={game.id}
+          key={game.id}
+          title={game.title}
+          turns={game.turns}
+          wPlayer={game.wplayer}
+          bPlayer={game.bplayer}
+          checkmate={game.checkmate}
+          resignation={game.resignation}
+          winner={game.winner}
+          description={game.description}
+          isUser={auth.isLoggedIn}
+          public={game.public}
+        />
+      );
+    });
+  }
 
-//   return (
-//     <React.Fragment>
-//       {content && <ul className='gamelist'>{content}</ul>}
-//       {!content && <h2>No games found! Go play some chess!</h2>}
-//     </React.Fragment>
-//   );
-// };
+  return (
+    <React.Fragment>
+      {content && <ul className='gamelist'>{content}</ul>}
+      {!content && <h2>No games found! Go play some chess!</h2>}
+    </React.Fragment>
+  );
+};
 
-// export default UserGames;
+export default UserGames;
