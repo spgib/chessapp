1bae59fe5a83a73e661e9d851dd242232f3430c3
Spencer <sp.gib3@gmail.com>
2022-02-13 14:54:39 +0100

    Added styling to error modal

--- chessapp/src/shared/components/UIElements/ErrorModal.js
+++ chessapp/src/shared/components/UIElements/ErrorModal.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Modal from './Modal';
+import Button from '../formElements/Button';
+
+import './ErrorModal.css';
 
 const ErrorModal = (props) => {
   return (
-    <Modal onClick={props.clear}>
+    <Modal className='error-modal' onClick={props.clear}>
       <h2>An error occurred!</h2>
       <p>{props.message}</p>
-      <button onClick={props.clear}>OK</button>
+      <Button onClick={props.clear}>OK</Button>
     </Modal>
   );
 };
