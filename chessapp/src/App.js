8eb4c5f3e0de3089a55eb49a8aac1ad1e2cddbac
Spencer <sp.gib3@gmail.com>
2022-02-27 17:23:39 +0100

    Changed public files and added code splitting

--- chessapp/src/App.js
+++ chessapp/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Layout from './shared/layout/Layout';
 import Main from './main/pages/Main';
-import PublicGames from './games/pages/PublicGames';
-import UserGames from './games/pages/UserGames';
-import Login from './user/pages/Login';
-import Signup from './user/pages/Signup';
+// import PublicGames from './games/pages/PublicGames';
+// import UserGames from './games/pages/UserGames';
+// import Login from './user/pages/Login';
+// import Signup from './user/pages/Signup';
 import { AuthContext } from './store/context/auth-context';
 import useAuth from './shared/hooks/useAuth';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+
+const PublicGames = React.lazy(() => import('./games/pages/PublicGames'));
+const UserGames = React.lazy(() => import('./games/pages/UserGames'));
+const Login = React.lazy(() => import('./user/pages/Login'));
+const Signup = React.lazy(() => import('./user/pages/Signup'));
 
 const App = () => {
   const [token, userId, username, login, logout] = useAuth();
@@ -19,14 +25,16 @@ const App = () => {
     >
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path='/public/:publicGameId' element={<Main />} />
-          <Route path='/user/:userGameId' element={<Main />} />
-          <Route path='/public' element={<PublicGames />} />
-          {token && <Route path='games/:uid' element={<UserGames />} />}
-          {!token && <Route path='login' element={<Login />} />}
-          {!token && <Route path='signup' element={<Signup />} />}
-          <Route path='*' element={<Navigate to='/' />} />
+          <Suspense fallback={<LoadingSpinner />}>
+            <Route index element={<Main />} />
+            <Route path='/public/:publicGameId' element={<Main />} />
+            <Route path='/user/:userGameId' element={<Main />} />
+            <Route path='/public' element={<PublicGames />} />
+            {token && <Route path='games/:uid' element={<UserGames />} />}
+            {!token && <Route path='login' element={<Login />} />}
+            {!token && <Route path='signup' element={<Signup />} />}
+            <Route path='*' element={<Navigate to='/' />} />
+          </Suspense>
         </Route>
       </Routes>
     </AuthContext.Provider>
