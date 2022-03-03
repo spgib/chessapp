99051a4b633974706259d008a6d6f74b00355003
Spencer <sp.gib3@gmail.com>
2022-03-03 18:11:29 +0100

    Began work on dockerizing backend

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,9 +3,53 @@ const pg = require('pg');
 class Pool {
   _pool = null;
 
-  connect(options) {
+  async connect(options) {
     this._pool = new pg.Pool(options);
-    return this._pool.query('SELECT 1 + 1;');
+
+    let retries = 5;
+    while (retries) {
+      try {
+        await this._pool.query('SELECT 1 + 1;');
+        break;
+      } catch (err) {
+        console.log(err);
+        retries -= 1;
+        await new Promise(res => setTimeout(res, 5000));
+      }
+    }
+
+    if (retries !== 0) {
+      return this._pool.query('SELECT 1 + 1;');
+    } else {
+      throw new Error('Could not connect to database!');
+    }
+  }
+
+  init() {
+    this._pool.query(`CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      username VARCHAR(50) NOT NULL UNIQUE,
+      email VARCHAR(50) NOT NULL UNIQUE,
+      password VARCHAR(100) NOT NULL
+    );`);
+
+    this._pool.query(`CREATE TABLE IF NOT EXISTS games (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      title VARCHAR(50) NOT NULL,
+      wplayer VARCHAR(50),
+      bplayer VARCHAR(50),
+      description VARCHAR(200),
+      turns INTEGER NOT NULL,
+      checkmate BOOLEAN NOT NULL,
+      resignation BOOLEAN NOT NULL,
+      winner VARCHAR(3) DEFAULT '',
+      public BOOLEAN NOT NULL,
+      string VARCHAR NOT NULL
+    );`);
   }
 
   close() {
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const app = require('./app');
 const pool = require('./db/pool');
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
 
+const { HOST, DB_PORT, DB, USER, PASSWORD, PORT } = process.env;
+console.log(process.env);
 pool
   .connect({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    HOST,
+    DB_PORT,
+    DB,
+    USER,
+    PASSWORD,
   })
   .then(() => {
-    app().listen(process.env.PORT || 5000, () => {
+    pool.init();
+    console.log('Initialized database.');
+  })
+  .then(() => {
+    app().listen(PORT || 5000, () => {
       console.log('Listening on port 5000.');
     });
   })
